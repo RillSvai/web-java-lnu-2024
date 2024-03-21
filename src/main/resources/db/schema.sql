8b6eed0a6923fa842ade3dea80b04dea053257cc
RillSvai <kirilkvas07@gmail.com>
2024-03-21 13:38:49 +0200

    update: extend database schema

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,14 +1,28 @@
 CREATE TABLE smartphones (
-     id BIGSERIAL PRIMARY KEY,
-     brand VARCHAR(255),
-     model VARCHAR(255),
-     os VARCHAR(255),
-     screen_size DOUBLE PRECISION,
-     resolution VARCHAR(255),
-     cpu_model VARCHAR(255),
-     ram INTEGER,
-     storage INTEGER,
-     battery_capacity INTEGER,
-     camera_resolution VARCHAR(255),
-     nfc_support BOOLEAN
+                             id BIGSERIAL PRIMARY KEY,
+                             brand VARCHAR(64) NOT NULL,
+                             model VARCHAR(128) UNIQUE NOT NULL,
+                             os VARCHAR(32) NOT NULL,
+                             screen_size DOUBLE PRECISION NOT NULL,
+                             resolution VARCHAR(32) NOT NULL,
+                             cpu_model VARCHAR(64) NOT NULL,
+                             ram INTEGER NOT NULL,
+                             storage INTEGER NOT NULL,
+                             battery_capacity INTEGER NOT NULL,
+                             camera_resolution VARCHAR(32) NOT NULL,
+                             nfc_support BOOLEAN NOT NULL
+);
+
+CREATE TABLE users
+(
+    id            BIGSERIAL PRIMARY KEY,
+    username      VARCHAR(32) UNIQUE NOT NULL,
+    password_hash VARCHAR(128)       NOT NULL,
+    is_admin      BOOLEAN            NOT NULL,
+    first_name    VARCHAR(64)        NOT NULL,
+    middle_name   VARCHAR(64)        NOT NULL,
+    last_name     VARCHAR(64)        NOT NULL,
+    phone         VARCHAR(16)        NOT NULL,
+    email         VARCHAR(32) UNIQUE NOT NULL,
+    info          TEXT
 );
